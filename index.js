58ae027295da471062f2a69a9a0602c08fa4eac5
Nagendra1432 <nagendramejari@gmail.com>
2024-09-28 23:47:57 +0530

    message

---
+++ index.js
@@ -0,0 +1,16 @@
+// function getbudget(arr){
+//     sum=0
+//     for(i of arr){
+//       sum=sum+i.budget
+
+//     }
+//          console.log(sum)
+// }
+// let arr=[ { name: "John", age: 21, budget: 23000 },
+//   { name: "Steve",  age: 32, budget: 40000 },
+//   { name: "Martin",  age: 16, budget: 2700 }
+// ]
+// getbudget(arr)
+
+
+// output:65700
